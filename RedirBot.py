7ac219ee033893d7d25c3959f719401315afa71d
Taiiwo Llort <taiiwo@localhost.localdomain>
2013-07-09 16:07:00 +0100

    Update: made smaller.

--- RedirBot.py
+++ RedirBot.py
@@ -1,11 +1,5 @@
-import socket #gets the socket libs ready
-s1 = socket(socket.AF_INET,socket.SOCK_STREAM)#makes a new socket called s1
-s2 = socket(socket.AF_INET,socket.SOCK_STREAM)#makes a new socket called s2
-s1.connect((server1,port))#Connects to s1
-s2.connect((server2,port))#connects to s2
-s1.send('user taiiwo 8 *\r\n')#logs in to s1
-s2.send('user taiiwo 8 *\r\n')#logs in to s2
-s1.send('join #channel1\r\n')#s1 joins channel 1
-while 1 == True:#makes an infinite loop
-	recv = s1.recv(1024)#fetches 1024 bits from IRC on s1
-	s2.send("privmsg #channel2 :" + recv + "\r\n")#sends that data to a channel on s2
+import socket
+s1,s2 = (socket(socket.AF_INET,socket.SOCK_STREAM),socket(socket.AF_INET,socket.SOCK_STREAM))
+cheat1 = (s1.connect((server1,port)) + s2.connect((server2,port)) + s1.send('user taiiwo 8 *\r\n') + s2.send('user taiiwo 8 *\r\n') + s1.send('join #channel1\r\n'))
+while l:
+	s2.send("privmsg #channel2 :" + str(s1.recv(1024)) + "\r\n")
